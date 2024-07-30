5298622fa3f0cd5cdf8262533361756a185c99d1
4lesDvran <91631392+4lesDvran@users.noreply.github.com>
2024-07-29 23:09:51 -0500

    Update numaut.py

--- graphs/numaut.py
+++ graphs/numaut.py
@@ -7,20 +7,15 @@ import tkinter as tk
 #Valores
 left = [1, 2, 3, 4, 5]
 height = [4, 2, 0, 3, 2]
-
-# labels for bars
 tick_label = ['1', '2', '3', '4 o más', 'Corporativo']
 
-# plotting a bar chart
+# dibujado
 plt.bar(left, height, tick_label = tick_label,
         width = 0.6, color = ['blue'])
 
-# naming the x-axis
+# grafica
 plt.xlabel('N. de autores', color = "green")
-# naming the y-axis
 plt.ylabel('N. de referencias', color = "red")
-# plot title
 plt.title('DIAGRAMA #2: REF. POR NÚMERO DE AUTORES', color = "blue", fontweight = 'bold')
 
-# function to show the plot
 plt.show()
